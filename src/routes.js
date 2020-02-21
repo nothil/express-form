275aff83aba072020a7459db1e5080d397f4c6be
nothile <nothile.ngcobo@umuzi.org>
2020-02-21 11:12:24 +0200

    added test

--- src/routes.js
+++ src/routes.js
@@ -3,36 +3,41 @@ const visitors = require('./api')
 const express = require('express')
 const app = express() //init app
 const path = require('path');
+var bodyParser = require('body-parser')
 
-app.use(express.urlencoded()); //load view engine
+//app.use(express.urlencoded()); //load view engine
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/', express.static(__dirname + '/'));
 app.set('view engine', 'pug')  
 
 
 //rendering the form to host 
 app.get('/new_Visitor', (req, res) =>{ 
-    res.sendFile('${_dirname} /index.html')})
+  res.sendFile('${_dirname} /index.html')})
 
 // creating the table
  
 createTable();
 
 // post which sends to database
- app.post('/action', function(req, res){
-     //console.log(JSON.stringify(req.body));
+ app.post('/action', async (req, res) =>{
+     console.log(req.body);
      const visitor = req.body.name;
      const assistant_name = req.body.assistant;
      const visitors_age = req.body.age;
      const time_of_visit = req.body.time;
      const date_of_visit =req.body.date;
      const comments =req.body.comment;
-     const id = addNewVisitor(visitor, visitors_age, date_of_visit, time_of_visit, assistant_name,comments);
-     res.render('newVisitor',{visitors: req.body,
-        id: id})
+     const id = await addNewVisitor(visitor, visitors_age, date_of_visit, time_of_visit, assistant_name,comments);
+     res.render('newVisitor',{visitor: req.body.name,
+                               assistant_name: req.body.assistant,
+                               visitors_age: req.body.age, 
+                               time_of_visit: req.body.time,
+                               date_of_visit: req.body.date,
+                               comments: req.body.comment,
+                                id: id})
     res.end();
   });
- 
-
 
 
 
