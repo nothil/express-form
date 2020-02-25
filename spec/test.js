2ca8661c89df4116f36bde19472a5b8c4ba21f8a
nothile <nothile.ngcobo@umuzi.org>
2020-02-25 15:38:33 +0200

    spec

--- spec/test.js
+++ spec/test.js
@@ -1,41 +1,30 @@
-const axios = require("axios");
-const fixture = require('./fixture');
 
-describe("Server", () => {
-    var html;
-    beforeAll(() => {
-        
-    });
-    
-    describe("GET /new_visitor", () => {
-        var data = {};
-        beforeAll((done) => {
-          
-            axios.get("http://127.0.0.1:8080/new-visitor", (error, response, body) => {
-                data.status = response.statusCode;
-                data.body = body;
-                done();
-            });
-        });
-        
-    });
-    describe("GET /action", () => {
-        var data = {};
-        beforeAll((done) => {
-            axios.post("http://localhost:3000/action", (error, response, body) => {
-                data.status = response.statusCode;
-                data.body = JSON.parse(body);
-                done();
-            });
-        });
-        it("Status 200", () => {
-            expect(data.status).toBe(500);
-        });
-        it("Body", () => {
-            expect(data.body.message).toBe("This is an error response");
-        });
+describe('express form testing', ()=>{
+    const axios = require('axios');
+    const readline = require('readline');
+    const fixture = require('./fixture')
+    const { Pool } = require('pg')
+
+
+    beforeEach(()=>{
+        server = require('../src/routes')
+      
+        const pool = new Pool();
     });
 
     
+    it('should return an html form', async (done)=>{
+        try {
+            const html = await axios.get("http://localhost:3000/new_Visitor")
+            expect(html.data).toEqual(fixture)
+        } catch (err) {
+            console.log(err)
+        }
+
+        done()
+    })
+
+})
+
+
     
-});
--- src/routes.js
+++ src/routes.js
@@ -41,5 +41,6 @@ createTable();
 
 
 
-app.listen(3000, () => console.log(` app listening on port ${3000}!`));
+const server = app.listen(3000, () => console.log(` app listening on port ${3000}!`));
 
+module.exports = server;
